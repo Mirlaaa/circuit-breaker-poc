61d4c4ced49e2bc296f7c8fa65e742617bd35513
Mirla Lopes <mirlaisabel11@gmail.com>
2024-11-21 11:45:08 -0300

    refactor/fix: Add another view to controll the request/ fix where the breaker is setup

--- core/views.py
+++ core/views.py
@@ -4,21 +4,25 @@ import requests
 from http import HTTPStatus
 from django.http import JsonResponse
 from core.circuit_breaker import breaker, CIRCUIT_BREAKER_ERROR
+from django.conf import settings
+
+HOST_SERVER = settings.HOST_SERVER
 
 @breaker
-def make_request(request):
-    try:
-        for i in range(0, 11):
-            endpoint = 'failure/' if i % 2 == 0 else 'slower_request/'
-            response = requests.get(endpoint, timeout=0.1)
-            response.raise_for_status()
-            return JsonResponse(response.json())
-    except CIRCUIT_BREAKER_ERROR:
-        return JsonResponse(data={"message":"Service is down"}, status=HTTPStatus.SERVICE_UNAVAILABLE)
+def call_endpoint():    
+    endpoint = 'failure'
+    
+    if breaker.current_state == "half-open":
+        if breaker.fail_counter >= 4:
+            endpoint = 'success'
 
+    response = requests.get(f'{HOST_SERVER}/{endpoint}/')
+    response.raise_for_status()
+
+    return response
 
 def failure(request):
-    return JsonResponse(data={"message":"Failure"}, status=HTTPStatus.OK)
+    return JsonResponse(data={"message":"Failure"}, status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
 def slower_request(request):
     random_value = random.randint(0,1)
@@ -31,3 +35,12 @@ def slower_request(request):
 def success(request):
     return JsonResponse(data={"message":"Successful request!"}, status=HTTPStatus.OK)
 
+def index(request):
+    try:
+        response = call_endpoint()
+        return JsonResponse(data=response.json(), status=response.status_code)
+    except CIRCUIT_BREAKER_ERROR:
+        return JsonResponse(data={"message":"Circuit breaker is open!"}, status=HTTPStatus.SERVICE_UNAVAILABLE)
+    except Exception as e:
+        print(f'Error: {e}')
+        return JsonResponse(data={"message":"Internal server error!"}, status=HTTPStatus.INTERNAL_SERVER_ERROR)
